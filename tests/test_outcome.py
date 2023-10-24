6ed3a23445c7fc442cb9cd933de1902c3e81eccb
Ayub Ian <iayub.mia@gmail.com>
2023-10-24 13:31:58 +0300

    Run tests on outcome class

---
+++ tests/test_outcome.py
@@ -0,0 +1,18 @@
+try:
+    from roulette import Outcome
+except ImportError as ie:
+    print("Error Importing Libraries: ")
+    print("Details: {}".format(ie))
+    exit(1)
+
+def test_outcome():
+    o1 = Outcome("Red", 1)
+    o2 = Outcome("Red", 1)
+    o3 = Outcome("Black", 2)
+    assert str(o1) == "Red (1:1)"
+    assert repr(o2) == "Outcome(name='Red', odds=1)"
+    assert o1 == o2
+    assert o1.odds == 1
+    assert o1.name == "Red"
+    assert o1 != o3
+    assert o2 != o3
